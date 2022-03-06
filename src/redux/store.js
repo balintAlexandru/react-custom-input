afa4d9c55b81d5f1102400563704527eb59a7b4a
Balint Alexandru <balintalex737@gmail.com>
2022-03-07 00:41:27 +0200

    add test

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import inputSlice from '../features/inputBasicSlice'
-
+//test
 export const store = configureStore({
   reducer: {
     input:inputSlice,
